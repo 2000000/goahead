d72cf9b3f0fb0996c3d68e0014dc038d03637b89
gaojie <jackgao@qiyi.com>
2018-03-31 13:40:40 +0800

    fix

--- src/upload.c
+++ src/upload.c
@@ -454,7 +454,7 @@ WebsHash websGetUpload(Webs *wp)
 
 PUBLIC void websUploadOpen()
 {
-    uploadDir = ME_GOAHEAD_UPLOAD_DIR;
+    uploadDir = "/tmp"; //ME_GOAHEAD_UPLOAD_DIR;
     if (*uploadDir == '\0') {
 #if ME_WIN_LIKE
         uploadDir = getenv("TEMP");
